0356e3951deb437ae270fd19cd0a7d5380a1a5d6
dardinierjames <dardinier.james@orange.fr>
2020-11-03 16:36:53 +0100

    flipFlapImage

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function setNewImage(){
+    document.getElementById('img1').src = 'assets/img/nature.jpg';
+}
+function setOldImage(){
+    document.getElementById('img1').src = 'assets/img/city.jpg';
+}
